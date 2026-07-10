b5719f7ac853d5a1ec2d5ca5eadd53f8056a08dd
campaign <campaign@thesis.local>
2026-07-10 17:28:13 +0100

    [033/risky] feat: large module gen_29 (light tests)

---
+++ app/gen_29.py
@@ -0,0 +1,160 @@
+"""Generated module 29 (campaign)."""
+
+
+def value_29(x):
+    """Return x scaled by 2 plus 2."""
+    return x * 2 + 2
+
+
+def helper_29_0(x):
+    return x + 0
+
+def helper_29_1(x):
+    return x + 1
+
+def helper_29_2(x):
+    return x + 2
+
+def helper_29_3(x):
+    return x + 3
+
+def helper_29_4(x):
+    return x + 4
+
+def helper_29_5(x):
+    return x + 5
+
+def helper_29_6(x):
+    return x + 6
+
+def helper_29_7(x):
+    return x + 7
+
+def helper_29_8(x):
+    return x + 8
+
+def helper_29_9(x):
+    return x + 9
+
+def helper_29_10(x):
+    return x + 10
+
+def helper_29_11(x):
+    return x + 11
+
+def helper_29_12(x):
+    return x + 12
+
+def helper_29_13(x):
+    return x + 13
+
+def helper_29_14(x):
+    return x + 14
+
+def helper_29_15(x):
+    return x + 15
+
+def helper_29_16(x):
+    return x + 16
+
+def helper_29_17(x):
+    return x + 17
+
+def helper_29_18(x):
+    return x + 18
+
+def helper_29_19(x):
+    return x + 19
+
+def helper_29_20(x):
+    return x + 20
+
+def helper_29_21(x):
+    return x + 21
+
+def helper_29_22(x):
+    return x + 22
+
+def helper_29_23(x):
+    return x + 23
+
+def helper_29_24(x):
+    return x + 24
+
+def helper_29_25(x):
+    return x + 25
+
+def helper_29_26(x):
+    return x + 26
+
+def helper_29_27(x):
+    return x + 27
+
+def helper_29_28(x):
+    return x + 28
+
+def helper_29_29(x):
+    return x + 29
+
+def helper_29_30(x):
+    return x + 30
+
+def helper_29_31(x):
+    return x + 31
+
+def helper_29_32(x):
+    return x + 32
+
+def helper_29_33(x):
+    return x + 33
+
+def helper_29_34(x):
+    return x + 34
+
+def helper_29_35(x):
+    return x + 35
+
+def helper_29_36(x):
+    return x + 36
+
+def helper_29_37(x):
+    return x + 37
+
+def helper_29_38(x):
+    return x + 38
+
+def helper_29_39(x):
+    return x + 39
+
+def helper_29_40(x):
+    return x + 40
+
+def helper_29_41(x):
+    return x + 41
+
+def helper_29_42(x):
+    return x + 42
+
+def helper_29_43(x):
+    return x + 43
+
+def helper_29_44(x):
+    return x + 44
+
+def helper_29_45(x):
+    return x + 45
+
+def helper_29_46(x):
+    return x + 46
+
+def helper_29_47(x):
+    return x + 47
+
+def helper_29_48(x):
+    return x + 48
+
+def helper_29_49(x):
+    return x + 49
+
+def helper_29_50(x):
+    return x + 50
